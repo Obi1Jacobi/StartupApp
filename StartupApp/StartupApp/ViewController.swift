52e590ed613b21852c4abbb43abb2ea30ae43ea2
Jacob Wilson <jacob@Jacobs-MacBook-Pro.local>
2016-04-25 16:25:16 -0500

    Project build

---
+++ StartupApp/StartupApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StartupApp
+//
+//  Created by Jacob Wilson on 4/25/16.
+//  Copyright © 2016 Jacob_Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
