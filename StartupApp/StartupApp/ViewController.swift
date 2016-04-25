009848f5eebe94564713ce88bd29402b1a069428
Jacob Wilson <jacob@Jacobs-MacBook-Pro.local>
2016-04-25 16:45:30 -0500

    Init Podfile
    Added in CocoaPods to the app, its actually super simple now I don’t
    know why I have never used it before. And started to begin work on
    parsing the data from forecast.io

--- StartupApp/StartupApp/ViewController.swift
+++ StartupApp/StartupApp/ViewController.swift
@@ -7,11 +7,33 @@
 //
 
 import UIKit
+import Firebase
+import ForecastIO
 
 class ViewController: UIViewController {
 
+    func test() {
+        let forecastIOClient = APIClient(apiKey: "3fd99f3c6b90149f3f3e7f599a47a5a6")
+        
+        let myLat = 40.6963
+        let myLon = -89.6166
+        
+        forecastIOClient.getForecast(latitude: myLat, longitude: myLon) { (currentForecast, error) -> Void in
+            if let currentForecast = currentForecast {
+                //  We got the current forecast!
+                print(currentForecast)
+            } else if let error = error {
+                //  Uh-oh we have an error!
+                print("There is an error: \(error)")
+            }
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.test()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
